5715b6ccaab6e434880f36eb945c098ad3a70991
Laboratório FIAP <labsfiap@fiap.com.br>
2026-05-12 20:45:48 -0300

    ResumoJava

--- AulasGeral.js
+++ AulasGeral.js
@@ -250,3 +250,287 @@ if (!isNaN(tabNum)) {
 
 
 
+//Palindromo
+
+let palavra = prompt("Digite uma palavra:").toLowerCase();
+
+let invertida = "";
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+    invertida += palavra[i];
+}
+
+if (palavra === invertida) {
+    document.write("A palavra <strong>" + palavra + "</strong> é um palíndromo.");
+} else {
+    document.write("A palavra <strong>" + palavra + "</strong> não é um palíndromo.");
+}
+
+
+//Par e impar
+
+let numero = Number(prompt("Digite um número:"));
+
+if (numero % 2 === 0) {
+    document.write("O número <strong>" + numero + "</strong> é PAR.");
+} else {
+    document.write("O número <strong>" + numero + "</strong> é ÍMPAR.");
+}
+
+
+//Numeros primos
+
+let numero = Number(prompt("Digite um número:"));
+let primo = true;
+
+if (numero <= 1) {
+    primo = false;
+}
+
+for (let i = 2; i < numero; i++) {
+    if (numero % i === 0) {
+        primo = false;
+    }
+}
+
+if (primo) {
+    document.write("O número <strong>" + numero + "</strong> é primo.");
+} else {
+    document.write("O número <strong>" + numero + "</strong> não é primo.");
+}
+
+//Caixa Eletronico
+
+let total = 0;
+let preco = 0;
+
+while (preco !== 0) {
+
+    preco = Number(prompt("Digite o preço do produto (0 para finalizar):"));
+
+    if (preco !== 0) {
+        total += preco;
+    }
+}
+
+document.write("<h2>Total da compra: R$ " + total.toFixed(2) + "</h2>");
+
+//Verificar numero negativo ou zero
+
+let numero = Number(prompt("Digite um número:"));
+
+if (numero > 0) {
+    document.write("Número positivo");
+} else if (numero < 0) {
+    document.write("Número negativo");
+} else {
+    document.write("Número igual a zero");
+}
+
+
+//Calcular média de duas nota
+
+let nota1 = Number(prompt("Digite a primeira nota:"));
+let nota2 = Number(prompt("Digite a segunda nota:"));
+
+let media = (nota1 + nota2) / 2;
+
+document.write("Média: " + media.toFixed(1));
+
+if (media >= 6) {
+    document.write("<br>Aprovado");
+} else {
+    document.write("<br>Reprovado");
+}
+
+
+//Verificar se um número é par ou ímpar
+
+let numero = Number(prompt("Digite um número:"));
+
+if (numero % 2 === 0) {
+    document.write("Número PAR");
+} else {
+    document.write("Número ÍMPAR");
+}
+
+
+//Tabuada de um número
+
+let numero = Number(prompt("Digite um número:"));
+
+for (let i = 1; i <= 10; i++) {
+    document.write(numero + " x " + i + " = " + (numero * i) + "<br>");
+}
+
+
+//Sistema simples de mercado
+
+let total = 0;
+let preco = 0;
+
+while (preco !== 0) {
+
+    preco = Number(prompt("Digite o preço do produto (0 para finalizar):"));
+
+    if (preco !== 0) {
+        total += preco;
+    }
+}
+
+document.write("Total da compra: R$ " + total.toFixed(2));
+
+
+//Verificar número primo
+
+let numero = Number(prompt("Digite um número:"));
+let primo = true;
+
+if (numero <= 1) {
+    primo = false;
+}
+
+for (let i = 2; i < numero; i++) {
+
+    if (numero % i === 0) {
+        primo = false;
+    }
+}
+
+if (primo) {
+    document.write("Número primo");
+} else {
+    document.write("Número não primo");
+}
+
+
+//Contagem regressiva
+
+for (let i = 10; i >= 0; i--) {
+    document.write(i + "<br>");
+}
+
+document.write("FIM!");
+
+
+//Calculadora simples
+
+let numero1 = Number(prompt("Digite o primeiro número:"));
+let numero2 = Number(prompt("Digite o segundo número:"));
+
+let operacao = prompt("Digite a operação (+, -, *, /)");
+
+if (operacao === "+") {
+    document.write("Resultado: " + (numero1 + numero2));
+} else if (operacao === "-") {
+    document.write("Resultado: " + (numero1 - numero2));
+} else if (operacao === "*") {
+    document.write("Resultado: " + (numero1 * numero2));
+} else if (operacao === "/") {
+    document.write("Resultado: " + (numero1 / numero2));
+} else {
+    document.write("Operação inválida");
+}
+
+//. Verificar maioridade
+
+let idade = Number(prompt("Digite sua idade:"));
+
+if (idade >= 18) {
+    document.write("Maior de idade");
+} else {
+    document.write("Menor de idade");
+}
+
+
+//Calculadora de desconto
+
+let valor = Number(prompt("Digite o valor da compra:"));
+
+if (valor >= 100) {
+    valor = valor * 0.9;
+}
+
+document.write("Valor final: R$ " + valor.toFixed(2));
+
+
+//Média de 4 notas
+
+let soma = 0;
+
+for (let i = 1; i <= 4; i++) {
+
+    let nota = Number(prompt("Digite a nota " + i + ":"));
+
+    soma += nota;
+}
+
+let media = soma / 4;
+
+document.write("Média final: " + media.toFixed(1));
+
+if (media >= 6) {
+    document.write("<br>Aprovado");
+} else {
+    document.write("<br>Reprovado");
+}
+
+
+//Contador de números pares
+
+let pares = 0;
+
+for (let i = 1; i <= 10; i++) {
+
+    let numero = Number(prompt("Digite um número:"));
+
+    if (numero % 2 === 0) {
+        pares++;
+    }
+}
+
+document.write("Quantidade de pares: " + pares);
+
+
+
+//Login
+
+let usuario = prompt("Digite o usuário:");
+let senha = prompt("Digite a senha:");
+
+if (usuario === "admin" && senha === "1234") {
+    document.write("Login realizado com sucesso");
+} else {
+    document.write("Usuário ou senha inválidos");
+}
+
+
+//Menu
+let opcao = 0;
+
+while (opcao !== 3) {
+
+    opcao = Number(prompt(
+        "MENU\n" +
+        "1 - Olá Mundo\n" +
+        "2 - Data Atual\n" +
+        "3 - Sair"
+    ));
+
+    if (opcao === 1) {
+
+        document.write("Olá Mundo<br>");
+
+    } else if (opcao === 2) {
+
+        document.write(new Date() + "<br>");
+
+    } else if (opcao === 3) {
+
+        document.write("Sistema encerrado");
+
+    } else {
+
+        document.write("Opção inválida<br>");
+    }
+}
